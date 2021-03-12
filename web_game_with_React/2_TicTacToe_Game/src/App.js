244778b6897214da5ca1eae75fb3538447e7f296
YUNA <favoryun12@gmail.com>
2021-03-12 10:52:42 +0900

    make react web game

---
+++ web_game_with_React/2_TicTacToe_Game/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Game from './component/Game';
+// import Square from './component/Square';
+// import Board from './component/Board';
+
+function App() {
+  return (
+    <div className='App'>
+      <Game />
+    </div>
+  );
+}
+
+export default App;
---
+++ web_game_with_React/README.md
@@ -0,0 +1,2 @@
+# web_game_with_React
+리액트로 웹게임을 만들어봅니다.
